8b52f4d3e4f0b7a7a1454df71c193539938999ac
Gabriel <g@>
2020-09-26 19:34:38 -0400

    table items

--- logic.js
+++ logic.js
@@ -1,1 +1,15 @@
-document.getElementById("banner").innerHTML += `<h4 id=DateEntry class=Custom> Today is ${new Date().toLocaleDateString()}</h4>`
+//add date to banner
+document.getElementById("banner").innerHTML += `<p id=DateEntry class=Custom> Today is ${new Date().toLocaleDateString()}</p>`
+
+
+//loop through items and add icon on screen
+getItems().forEach(element => {
+    document.getElementById("itemTable").innerHTML += `<div class="tableItem"><p>item: ${element} </p><div>`
+});
+
+
+
+function getItems(){
+    var items = ["first", "second", "third"];
+    return items;
+}
